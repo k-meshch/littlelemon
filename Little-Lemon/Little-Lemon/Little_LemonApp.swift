8d174a5b9916a19ea635d6fe6df4baf5d890e800
Ksenia Meshcheryakova <meshch2018@gmail.com>
2024-10-09 17:59:23 +0300

    Initial commit of Little Lemon app

---
+++ Little-Lemon/Little-Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little-Lemon
+//
+//  Created by Ksenia Meshch on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
